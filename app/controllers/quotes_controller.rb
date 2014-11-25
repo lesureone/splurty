86c38bb44bd4be47a3359f07aa1180489a913d94
Larry LeSure <lesure1@gmail.com>
2014-11-25 14:01:56 +0000

    Moved the quotes form to a bootstrap modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,15 +3,11 @@ class QuotesController < ApplicationController
 		@quote = Quote.order("RANDOM()").first
 	end
 
-	def new
-		@quote = Quote.new
-	end
-
 	def create
 		@quote = Quote.create(quote_params)
 		if @quote.invalid?
 			flash[:error] = "<strong> Could not save</strong> the data you entered is invalid."
-		end
+	end
 
 		redirect_to root_path
 	end
