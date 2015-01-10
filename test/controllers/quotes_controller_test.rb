6aeb3aa1a29c90fb9813caec98e654176b109161
Larry LeSure <lesure1@gmail.com>
2015-01-10 21:41:53 +0000

    Updated the styling

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "quote show page" do
+     quote = Quote.create(:saying => "Cash rules everything around me, cream gets the money,dolla-dolla bill yall", :author => 'WuTang Clan')
+		get :show, :id => quote.id
+		assert_response :success
+   end
+	
+	test "quote show page, not found"  do
+		get :show, :id => 'OMG'
+		assert_response :not_found
+	end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "unique_tag" do
+		quote = Quote.create(:saying => "Cash rules everything around me, cream gets the money,dolla-dolla bill yall", :author => 'WuTang Clan')
+		expected = 'WC#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
 end
